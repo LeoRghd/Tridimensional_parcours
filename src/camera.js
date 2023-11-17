347cdf2daeea6ae2d0b3333867f845a1acbaea9f
AdlanBousehaba <91885383+AdlanBousehaba@users.noreply.github.com>
2023-11-17 14:44:34 +0100

    camera follow, sphere control
    Co-authored-by: Léo Rangheard <LeoRghd@users.noreply.github.com>

--- src/camera.js
+++ src/camera.js
@@ -1,11 +1,23 @@
 var createCamera = function (scene, Targets) {
   const camera = new BABYLON.FreeCamera("camera1", 
-      new BABYLON.Vector3(50, 50, 50), 
+      new BABYLON.Vector3(0, 50, 0), 
       scene);
   // Targets the camera to scene origin
-  camera.setTarget(new BABYLON.Vector3(0, 50, 0));
+  camera.setTarget(new BABYLON.Vector3(0, 0, 0));
   // Attaches the camera to the canvas
   camera.attachControl(canvas, true);
+
+  return camera;
+}
+
+function createFollowCamera(scene, target) {
+  const camera = new BABYLON.FollowCamera('followCamera', new BABYLON.Vector3(0, 10, -20), scene);
+  camera.radius = 10;
+  camera.heightOffset = 5;
+  camera.rotationOffset = 1;
+  camera.cameraAcceleration = 0.1;
+  camera.maxCameraSpeed = 5;
+  camera.lockedTarget = target;
 
   return camera;
 }
--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
-
 var canvas = document.getElementById("renderCanvas");
 var engine = new BABYLON.Engine(canvas, true);
 
 const createScene = function () {
   // Creates a basic Babylon Scene object
   const scene = new BABYLON.Scene(engine);
+
   // Creates and positions a free camera
   var sphere = createSphere(scene, 2, 30, 10);
-  var sphere2 = createSphere(scene, 10, 0, 20, 0)
-  var camera = createCamera(scene, sphere2)
-  var light = createLight(scene)
+  var character = createSphere(scene, 10, 0, 20, 0, "character", true);
+
+  var camera = createCamera(scene, character);
+  var light = createLight(scene);
   var ground = CreateGround(scene);
+
+
 
   const texture = new BABYLON.Texture("./utils/img/guts_dodo.jpg", scene);
   const material = new BABYLON.StandardMaterial("material", scene);
@@ -19,10 +22,12 @@ const createScene = function () {
 
   return scene;
 };
+
+
 var scene = createScene();
 
 engine.runRenderLoop(function () {
-    scene.render();
+  scene.render();
 });
 
 window.addEventListener("resize", function () {
